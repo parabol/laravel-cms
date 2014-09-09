75f45d50b2308cd2350b5a917704a5fdebaf2c46
Leo Nguyen <nam85it@gmail.com>
2014-09-09 09:06:11 +0700

    tinymce

--- app/modules/backend/views/pages/_form.blade.php
+++ app/modules/backend/views/pages/_form.blade.php
@@ -12,7 +12,7 @@
 </div>
 <div class="form-group">
     {{ Form::label('content', 'Content:') }}
-    {{ Form::textarea('content', null, array('class' => 'form-control')) }}
+    {{ Form::textarea('content', null, array('id' => 'content','class' => 'form-control')) }}
 </div>
 <div class="form-group">
     {{ Form::label('title_tag', 'Title Tag:') }}
@@ -30,3 +30,18 @@
     {{ Form::label('status', 'Status:') }}
     {{ Form::text('status', null, array('class' => 'form-control')) }}
 </div>
+<script src="{{ asset('packages/zofe/rapyd/assets/tinymce/tinymce.min.js') }}"></script>
+<script type="text/javascript">
+tinymce.init({
+    selector: 'textarea#content',
+    plugins: [
+         'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+         'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+         'save table contextmenu directionality emoticons template paste textcolor responsivefilemanager'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | responsivefilemanager | print preview media fullpage | forecolor backcolor emoticons', 
+    image_advtab: true,
+    external_filemanager_path: '{{ URL::to('/') }}/packages/filemanager/',
+    filemanager_title: 'Upload',
+});
+</script>
